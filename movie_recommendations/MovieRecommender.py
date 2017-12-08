ccc1d08ca6a6c5c5ac72e076fef335617077308f
Zuhaib Ahmad <xuhaib_ahmad@yahoo.com>
2017-12-08 16:14:49 +0500

    Move genre prefs to json file

--- movie_recommendations/MovieRecommender.py
+++ movie_recommendations/MovieRecommender.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 from movie_recommendations.config import Constants
 
 conn = http.client.HTTPSConnection(Constants.API_ENDPOINT)
+genre = []
 
 # Fetch last used position
 last_position = 0
@@ -16,12 +17,20 @@ try:
 except FileNotFoundError:
     raise FileNotFoundError(Constants.ERROR_KEY_FILE_NOT_FOUND.format(Constants.API_KEY_FILENAME))
 
+# Read genre prefs from JSON file
+try:
+    with open(Constants.SETTINGS_FILENAME) as settings_file:
+        settings = json.load(settings_file)
+        genre = settings["genre"]
+except FileNotFoundError:
+    raise FileNotFoundError(Constants.ERROR_KEY_FILE_NOT_FOUND.format(Constants.API_KEY_FILENAME))
+
 # Fetch genre IDs
 conn.request("GET", Constants.URL_GENRES.format(api_key))
 response = conn.getresponse().read()
 data = json.loads(response, object_hook=lambda d: namedtuple('genre', d.keys())(*d.values()))
-genre_names = [g.name for g in data.genres if str(g.name) in Constants.GENRE]
-genre_ids = [g.id for g in data.genres if str(g.name) in Constants.GENRE]
+genre_names = [g.name for g in data.genres if str(g.name) in genre]
+genre_ids = [g.id for g in data.genres if str(g.name) in genre]
 print("SELECTED GENRE NAME: {0}".format(genre_names))
 
 # Fetch top 3 movies in defined categories
